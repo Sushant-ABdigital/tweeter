46be0dfd4663f02d261d134c92a8697bd0918a17
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-11-07 02:46:17 +0000

    Completed basic functionality - design points remaining

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,32 +7,38 @@
  */
 
 //Helper Data
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1461113959088
-  }
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac"
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     created_at: 1461116232227
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd"
+//     },
+//     content: {
+//       text: "Je pense , donc je suis"
+//     },
+//     created_at: 1461113959088
+//   }
+// ];
 
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 //Function to create mark up of tweet.
+
 const createTweetElement = data => {
   return `<section class="tweets">
       <article class="tweet">
@@ -42,7 +48,7 @@ const createTweetElement = data => {
           <h6>${data.user.handle}</h6>
         </header>
         <section class="body">
-          <p>${data.content.text}</p>
+          <p>${escape(data.content.text)}</p>
         </section>
         <footer>
           <p>${data.created_at}</p>
@@ -56,15 +62,103 @@ const createTweetElement = data => {
     </section>`;
 };
 
-//Function to create tweet
+//Function to create tweets
 const renderTweets = data => {
+  //Remove everything from the container to prevent multiple updates.
+  $(".tweet-container").empty();
   data.forEach(el => {
     const tweet = createTweetElement(el);
-    $(".container").append(tweet);
+    $(".tweet-container").prepend(tweet);
   });
 };
 
 //Appending to the DOM after DOM is ready.
 $(document).ready(() => {
-  renderTweets(data);
+  //Go to Top functionality...
+  $(window).scroll(function() {
+    console.log("ran");
+    if ($(this).scrollTop() > 500) {
+      $(".container_top").fadeIn();
+    } else {
+      $(".container_top").fadeOut();
+    }
+  });
+
+  $(".container_top").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      600
+    );
+    return false;
+  });
+  //Creating function to fetch the data from http://localhost:8080/tweets ;
+  const loadTweets = () => {
+    $.ajax({
+      method: "GET",
+      url: "http://localhost:8080/tweets",
+      dataType: "JSON",
+      success: function(data) {
+        renderTweets(data);
+      },
+      error: function(err) {
+        console.log("errored!", err);
+      }
+    });
+  };
+
+  loadTweets();
+
+  //Form submission
+  $("#tweeter_form").submit(function(e) {
+    //Preventing the default behaviour of submitting the form
+    e.preventDefault();
+
+    //validation of form input
+    const userInput = $(".user_input").val();
+    if (!userInput) {
+      $(".error_msg")
+        .text("Tweet can not be empty")
+        .slideDown("fast")
+        .delay(3000)
+        .slideUp("fast");
+      return;
+    } else if (userInput.length > 140) {
+      $(".error_msg")
+        .text("Tweets can not be longer than 140 characters!")
+        .slideDown("fast")
+        .delay(3000)
+        .slideUp("fast");
+      return;
+    } else {
+      $(".error_msg").hide();
+    }
+    // Getting the serialized value
+    const val = $(this).serialize();
+    //Making post request
+    $.ajax({
+      method: "POST",
+      data: val,
+      url: "/tweets",
+      success: function() {
+        //Upon successful submission fetch data to render
+        loadTweets();
+      },
+      error: function() {
+        console.log("error occured");
+      }
+    });
+  });
+
+  //Animation of form
+  $(".nav_content a").click(function() {
+    if ($(".new-tweet").is(":hidden")) {
+      $(".new-tweet").slideDown(1000, function() {
+        $(".user_input").focus();
+      });
+    } else {
+      $(".new-tweet").hide();
+    }
+  });
 });
